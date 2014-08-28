171fa2e1ac40d1318c6bdcd9322b2aa65aeb7e3c
ksr218 <ksr218@lehigh.edu>
2014-08-28 18:36:30 +0000

    Updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,21 @@
+//define class
+public class HelloWorld{
+    
+    //add main method
+    public static void main(String[] args) {
+        
+        //print the statement
+        System.out.println("#####################");
+        System.out.println("#####################");
+        System.out.println("#                   #");
+        System.out.println("# Hello World 2014! #");
+        System.out.println("#                   #");
+        System.out.println("#####################");
+        System.out.println("#####################");
+        System.out.println("test");
+        
+    }
+}
+    
+ 
+       
