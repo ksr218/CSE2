31559c9a905b064c42989662b4c610b50f1cd996
ksr218 <ksr218@lehigh.edu>
2014-08-29 13:23:22 +0000

    Updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,23 @@
+//Keith Edward Runes
+//hw01
+//WelcomeClass Java Program
+
+//define the class
+public class WelcomeClass{
+    
+    //add main method
+    public static void main(String[] args) {
+        
+        //introduce yourself and stuff
+        System.out.println("--------------------");
+        System.out.println("|      WELCOME     |");
+        System.out.println("--------------------");
+        System.out.println("  ^  ^  ^  ^  ^  ^  ");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("|-k--s--r--2--1--8-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("  v  v  v  v  v  v  ");
+        System.out.println("I can play the violin, acoustic and bass guitar, and the keyboard.");
+        
+    }
+}
